471ca8cd5309c567e0ef4257f2786286457f8067
0alif <alifhossain003@gmail.com>
2021-09-06 02:00:31 +0600

    first commit

---
+++ summary.js
@@ -0,0 +1,16 @@
+// alert('haw are you')
+// confirm('are you single')
+// prompt('name')
+
+// localStorage.reload()
+// location.href = ''
+// location.hash =
+// https://google.com
+
+// history.back()
+// history.go()
+
+// cookies
+
+// sessionStorage vs localStorage
+// cookies vs sessionStorage vs localStorage
